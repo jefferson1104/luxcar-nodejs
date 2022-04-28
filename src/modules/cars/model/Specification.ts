64304c0ebae91003e46229c0eaee1b87df0a8238
jefferson1104 <jefferson1104junior@gmail.com>
2022-04-28 02:18:42 -0300

    creating specification service and separating into modules

---
+++ src/modules/cars/model/Specification.ts
@@ -0,0 +1,16 @@
+import { v4 as uudiV4 } from "uuid";
+
+class Specification {
+  id?: string;
+  name: string;
+  description: string;
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uudiV4();
+    }
+  }
+}
+
+export { Specification };
