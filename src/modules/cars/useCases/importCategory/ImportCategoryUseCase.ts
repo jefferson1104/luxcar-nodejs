20ca41b7a5cf29e4909851d2c252f961f27dc23d
jefferson1104 <jefferson1104junior@gmail.com>
2022-05-12 23:50:47 -0300

    stream and csv parse

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,7 +1,19 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { parse } from "csv-parse";
+import fs from "fs";
+
 class ImportCategoryUseCase {
-  execute(file: any) {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = parse({
+      delimiter: ",",
+    });
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
