62346f9304112bd87977f6093e2a457b1562a77d
jefferson1104 <jefferson1104junior@gmail.com>
2022-06-21 19:37:12 -0300

    route authentication and error handling

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
 const specificationsRoutes = Router();
 
 const createSepecificationController = new CreateSpecificationController();
 
+specificationsRoutes.use(ensureAuthenticated);
 specificationsRoutes.post("/", createSepecificationController.handle);
 
 export { specificationsRoutes };
