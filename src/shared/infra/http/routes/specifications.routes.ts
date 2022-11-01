ba3e0fa52f814465c51f65c152785d095051623f
Jefferson Soares <jefferson1104junior@gmail.com>
2022-11-01 00:30:26 -0300

    creating admin middleware

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
 
 import { CreateSpecificationController } from "@modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
 
 const createSepecificationController = new CreateSpecificationController();
 
-specificationsRoutes.use(ensureAuthenticated);
-specificationsRoutes.post("/", createSepecificationController.handle);
+specificationsRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createSepecificationController.handle
+);
 
 export { specificationsRoutes };
