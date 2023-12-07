17d7f794861f223691c1707e329e7cc8f3092a5e
Jefferson Soares <jefferson1104junior@gmail.com>
2023-12-07 08:43:54 -0300

    create Rentals migration

--- src/shared/infra/typeorm/migrations/1701947247745-CreateRentals.ts
+++ src/shared/infra/typeorm/migrations/1701947247745-CreateRentals.ts
@@ -1,4 +1,4 @@
-import {MigrationInterface, QueryRunner, Table} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class CreateRentals1701947247745 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
