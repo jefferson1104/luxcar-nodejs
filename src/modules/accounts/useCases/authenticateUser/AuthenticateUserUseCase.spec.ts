922293e51e3d5b40b7815de89f9ca13d9f0946cd
Jefferson Soares <jefferson1104junior@gmail.com>
2023-12-14 17:51:39 -0300

    fix unit tests

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -10,54 +10,52 @@ let usersRepositoryInMemory: UsersRepositoryInMemory;
 let createUserUseCase: CreateUserUseCase;
 
 describe("Autheticate User", () => {
-  beforeEach(() => {
-    usersRepositoryInMemory = new UsersRepositoryInMemory();
-    authenticateUserUseCase = new AuthenticateUserUseCase(
-      usersRepositoryInMemory
-    );
-    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
-  });
+	beforeEach(() => {
+		usersRepositoryInMemory = new UsersRepositoryInMemory();
+		authenticateUserUseCase = new AuthenticateUserUseCase(
+		usersRepositoryInMemory
+		);
+		createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+	});
 
-  it("should be able to authenticate an user", async () => {
-    const user: ICreateUserDTO = {
-      driver_license: "0012345",
-      email: "user@test.com",
-      password: "1234",
-      name: "User Test",
-    };
-    await createUserUseCase.execute(user);
+	it("should be able to authenticate an user", async () => {
+		const user: ICreateUserDTO = {
+			driver_license: "0012345",
+			email: "user@test.com",
+			password: "1234",
+			name: "User Test",
+		};
 
-    const result = await authenticateUserUseCase.execute({
-      email: user.email,
-      password: user.password,
-    });
+		await createUserUseCase.execute(user);
 
-    expect(result).toHaveProperty("token");
-  });
+		const result = await authenticateUserUseCase.execute({
+			email: user.email,
+			password: user.password,
+		});
 
-  it("should not be able to authenticate an nonexistent user", async () => {
-    expect(async () => {
-      await authenticateUserUseCase.execute({
-        email: "false@email.com",
-        password: "1234",
-      });
-    }).rejects.toBeInstanceOf(AppError);
-  });
+		expect(result).toHaveProperty("token");
+	});
 
-  it("should not be able to authenticate with incorrect password", async () => {
-    expect(async () => {
-      const user: ICreateUserDTO = {
-        driver_license: "9999123",
-        email: "user@examplemail.com",
-        password: "1234",
-        name: "User Test Error",
-      };
-      await createUserUseCase.execute(user);
+	it("should not be able to authenticate an nonexistent user", async () => {
+		await expect( authenticateUserUseCase.execute({
+			email: "false@email.com",
+			password: "1234",
+		})).rejects.toEqual(new AppError("Email or password incorrect!"));
+	});
 
-      await authenticateUserUseCase.execute({
-        email: user.email,
-        password: "incorrectPass",
-      });
-    }).rejects.toBeInstanceOf(AppError);
-  });
+	it("should not be able to authenticate with incorrect password", async () => {
+		const user: ICreateUserDTO = {
+			driver_license: "9999123",
+			email: "user@examplemail.com",
+			password: "1234",
+			name: "User Test Error",
+		};
+
+		await createUserUseCase.execute(user);
+
+		await expect(authenticateUserUseCase.execute({
+			email: user.email,
+			password: "incorrectPass",
+		})).rejects.toEqual(new AppError("Email or password incorrect!"));
+	});
 });
--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,12 +1,12 @@
 import dayjs from "dayjs";
 
-import { CreateRentalUseCase } from "./CreateRentalUseCase"
-
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
 import { RentalsRepositoryInMemory } from "@modules/rentals/repositories/in-memory/RentalsRepositoryInMemory";
 
 import { AppError } from "@shared/errors/AppError";
 import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+
+import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
 let createRentalUseCase: CreateRentalUseCase;
 let dayjsProvider: DayjsDateProvider;
@@ -49,44 +49,38 @@ describe("Create Rental", () => {
 	});
 
 	it("Should not be able to create a new rental if is another open to the same user", async () => {
-		expect(async () => {
-			await createRentalUseCase.execute({
-				user_id: "12345",
-				car_id: "112233",
-				expected_return_date: dayAdd24Hours,
-			});
+		await rentalsRepositoryInMemory.create({
+			car_id: "123456",
+			expected_return_date: dayAdd24Hours,
+			user_id: "123456",
+		});
 
-			await createRentalUseCase.execute({
-				user_id: "12345",
-				car_id: "112233",
-				expected_return_date: dayAdd24Hours,
-			});
-		}).rejects.toBeInstanceOf(AppError);
+		await expect(createRentalUseCase.execute({
+			user_id: "123456",
+			car_id: "654321",
+			expected_return_date: dayAdd24Hours,
+		})).rejects.toEqual(new AppError("There's a rental in progress for user!"));
 	});
 
 	it("Should not be able to create a new rental if is another open to the same car", async () => {
-		expect(async () => {
-			await createRentalUseCase.execute({
-				user_id: "123",
-				car_id: "testCarA",
-				expected_return_date: dayAdd24Hours,
-			});
+		await rentalsRepositoryInMemory.create({
+			user_id: "123",
+			car_id: "testCarA",
+			expected_return_date: dayAdd24Hours,
+		});
 
-			await createRentalUseCase.execute({
-				user_id: "321",
-				car_id: "testCarA",
-				expected_return_date: dayAdd24Hours,
-			});
-		}).rejects.toBeInstanceOf(AppError);
+		await expect(createRentalUseCase.execute({
+			user_id: "321",
+			car_id: "testCarA",
+			expected_return_date: dayAdd24Hours,
+		})).rejects.toEqual(new AppError("Car is unavailable"));
 	});
 
 	it("Should not be able to create a new rental if with invalid return time", async () => {
-		expect(async () => {
-			await createRentalUseCase.execute({
-				user_id: "123",
-				car_id: "testCarA",
-				expected_return_date: dayjs().toDate(),
-			});
-		}).rejects.toBeInstanceOf(AppError);
+		await expect(createRentalUseCase.execute({
+			user_id: "123",
+			car_id: "testCarA",
+			expected_return_date: dayjs().toDate(),
+		})).rejects.toEqual(new AppError("Invalid return time!"));
 	});
 })
