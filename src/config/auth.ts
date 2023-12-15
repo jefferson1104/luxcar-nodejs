bcb06941c4cc8589cce5298e8b46df2ac8b1312b
Jefferson Soares <jefferson1104junior@gmail.com>
2023-12-15 13:11:38 -0300

    refact authenticate user

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+	secret_token: "d3d17e025949c173d40074ad60001a5c",
+	secret_refresh_token: "89610686dc1f323c173f96e0704d0946",
+	expires_in_token: "15m", // 15 minutes
+	expires_in_refresh_token: "30d", // 30 days
+	expires_refresh_token_days: 30,
+}
