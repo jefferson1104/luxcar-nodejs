86c85451ebf867c811907bdf6f6f243a3a046e75
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-04-27 02:22:16 -0300

    feature: implementing reset password

--- src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -6,41 +6,41 @@ import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepositor
 import { User } from "../entities/User";
 
 class UsersRepository implements IUsersRepository {
-  private repository: Repository<User>;
+	private repository: Repository<User>;
 
-  constructor() {
-    this.repository = getRepository(User);
-  }
+	constructor() {
+		this.repository = getRepository(User);
+	}
 
-  async create({
-    name,
-    email,
-    password,
-    driver_license,
-    avatar,
-    id,
-  }: ICreateUserDTO): Promise<void> {
-    const user = this.repository.create({
-      name,
-      email,
-      password,
-      driver_license,
-      avatar,
-      id,
-    });
+	async create({
+		name,
+		email,
+		password,
+		driver_license,
+		avatar,
+		id,
+	}: ICreateUserDTO): Promise<void> {
+		const user = this.repository.create({
+			name,
+			email,
+			password,
+			driver_license,
+			avatar,
+			id,
+		});
 
-    await this.repository.save(user);
-  }
+		await this.repository.save(user);
+	}
 
-  async findByEmail(email: string): Promise<User> {
-    const user = await this.repository.findOne({ email });
-    return user;
-  }
+	async findByEmail(email: string): Promise<User> {
+		const user = await this.repository.findOne({ email });
+		return user;
+	}
 
-  async findById(id: string): Promise<User> {
-    const user = await this.repository.findOne(id);
-    return user;
-  }
+	async findById(id: string): Promise<User> {
+		const user = await this.repository.findOne(id);
+		return user;
+	}
 }
 
 export { UsersRepository };
