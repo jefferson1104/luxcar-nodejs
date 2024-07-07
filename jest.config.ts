8a4226193aaf7644caddc21643899b9e0dfc38dc
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-07-07 01:23:03 -0300

    config: jest coverage tests

--- jest.config.ts
+++ jest.config.ts
@@ -3,12 +3,16 @@ import { pathsToModuleNameMapper } from "ts-jest/dist";
 import { compilerOptions } from "./tsconfig.json";
 
 export default {
-  bail: true,
-  clearMocks: true,
-  coverageProvider: "v8",
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
-    prefix: "<rootDir>/src/",
-  }),
-  preset: "ts-jest",
-  testMatch: ["**/*.spec.ts"],
+	bail: true,
+	clearMocks: true,
+	coverageProvider: "v8",
+	collectCoverage: true,
+	collectCoverageFrom: ["<rootDir>/src/modules/**/useCases/**/*.ts"],
+	coverageDirectory: "coverage",
+	coverageReporters: ["text-summary", "lcov"],
+	moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+		prefix: "<rootDir>/src/",
+	}),
+	preset: "ts-jest",
+	testMatch: ["**/*.spec.ts"],
 };
