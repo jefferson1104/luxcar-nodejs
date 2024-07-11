41852c7b8da638be1f27c72e92a665f9915638eb
Jefferson Soares <48356215+jefferson1104@users.noreply.github.com>
2024-07-10 22:32:59 -0300

    upload aws s3

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,7 +4,7 @@ import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
-// import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
 import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
@@ -18,7 +18,12 @@ container.registerInstance<IMailProvider>(
 	new EtherealMailProvider()
 );
 
+const diskStorage = {
+	local: LocalStorageProvider,
+	aws: S3StorageProvider,
+}
+
 container.registerSingleton<IStorageProvider>(
 	"StorageProvider",
-	S3StorageProvider
+	diskStorage[process.env.DISK]
 );
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import "reflect-metadata";
 import "express-async-errors";
 import "@shared/container";
@@ -21,18 +22,18 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
-      });
-    }
+	(err: Error, request: Request, response: Response, next: NextFunction) => {
+		if (err instanceof AppError) {
+			return response.status(err.statusCode).json({
+				message: err.message,
+			});
+		}
 
-    return response.status(500).json({
-      status: "error",
-      message: `Internal server error - ${err.message}`,
-    });
-  }
+		return response.status(500).json({
+			status: "error",
+			message: `Internal server error - ${err.message}`,
+		});
+	}
 );
 
 export { app };
